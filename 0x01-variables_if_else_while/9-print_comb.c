ec401e2dd739b42e5eb54e406389c56e7a43929a
dorothy <dorothycheruiyot97@gmail.com>
2022-03-12 01:37:48 +0300

    script modified

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,26 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
-* main - Print all possible combinations of single digit numbers
-*
-* Return: Always 0 (Success)
-*/
+ * main - Print single digit numbers in the formart
+ *
+ * Description: Sepaeated by commas
+ * Return: Always(0) Success
+ */
+
 int main(void)
 {
-		int number;
+	int i;
 
-		for (number = 0; number <= 9; number++)
-		{
-		putchar(number + '0');
-		if (number < 9)
-		{
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	if (i != 57)
+	{
 		putchar(',');
 		putchar(' ');
-		}
-		}
-		putchar('\n');
+	}
+	putchar('\n');
 
 	return (0);
 }
