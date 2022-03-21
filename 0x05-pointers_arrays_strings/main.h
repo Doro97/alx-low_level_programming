44dfe09309a69133633347cefd6c598e6bc9aa8f
Dorothy <dorothycheruiyot97@gmail.com>
2022-03-21 20:49:02 +0000

    compiled programs

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #define _MAIN_H_
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -11,4 +12,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-#endif
+
