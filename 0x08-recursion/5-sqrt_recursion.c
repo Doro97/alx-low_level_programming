1a15ae46d03d069a406494dc221023b11550c501
Dorothy <dorothycheruiyot97@gmail.com>
2022-03-30 20:32:30 +0000

    function modified

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,17 +7,33 @@
 *Return: the square root of the number
 */
 
-int _sqrt_recursion(int n)
+int sqtRecursive(int start, int end, int m)
 {
-	if(n < 0) 
-	{
-		return (-1);
-	}
-	
-	if (n == 1)
-	{
-		return (1);
-	}
-	return sqrt(n);
+	long mid;
 
+	if (end >= start)
+	{
+		mid = start + (end - start) / 2;
+		if (mid * mid == m)
+			return (mid);
+/* following binary search */
+		if (mid * mid > m)
+			return (sqtRecursive(start, mid - 1, m));
+		if (mid * mid < m)
+			return (sqtRecursive(mid + 1, end, m));
+	}
+	return (-1);
+}
+/**
+ * _sqrt_recursion - finds the natural square root of a number
+ * @n: given number
+ * Return: square root of n or -1
+ **/
+	int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (sqtRecursive(2, n, n));
 }
