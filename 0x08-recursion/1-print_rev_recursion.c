2d0060586a6fb284ec0ce7d95eb58e122eae9a11
Dorothy <dorothycheruiyot97@gmail.com>
2022-03-30 20:24:41 +0000

    functions modified

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,12 +5,11 @@
 *Return: nothing
 */
 
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		
-		_print_rev_recursion(s+1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
