cc6567d9ef5f50a918ef1a3f2071807236620bca
Dorothy <dorothycheruiyot97@gmail.com>
2022-03-30 19:24:12 +0000

    functions added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*_puts_recurssion-function that prints a string
+*@s:pointer to a string
+*Return:nothinf
+*/
+
+void _puts_recursion(char *s):
+{
+	if(*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	
+	_putchar(*s);
+	_puts_recursion(s+1);
+
+
+}
+
+
