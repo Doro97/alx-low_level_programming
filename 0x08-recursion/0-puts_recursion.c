4e7ee33962c9d55237dfce544b277bffe3a4e116
Dorothy <dorothycheruiyot97@gmail.com>
2022-03-30 20:20:07 +0000

    function added

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,18 +5,16 @@
 *Return:nothinf
 */
 
-void _puts_recursion(char *s):
+
+void _puts_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	
 	_putchar(*s);
-	_puts_recursion(s+1);
-
-
+	_puts_recursion(s + 1);
 }
 
 
